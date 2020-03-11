326bcfcffee966df45f7cf6aa83913b7689696a8
Yves Matta <yves.matta@qochealth.com>
2020-03-11 14:09:49 -0400

    update tests

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,6 +1,6 @@
 describe('Example', () => {
-  beforeEach(async () => {
-    await device.reloadReactNative();
+  beforeAll(async () => {
+    await device.launchApp({ newInstance: true })
   });
 
   it('should have welcome screen', async () => {
